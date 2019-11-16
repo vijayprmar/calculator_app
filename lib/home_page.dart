8a61a11ba9f72ace02f670f3ac2f11f70689454d
Vijay Parmar <vsparmar1994@gmail.com>
2019-11-16 16:40:22 +0530

    Initial Commit For Flutter

---
+++ lib/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget{
+
+  @override
+  State createState()  => new HomePageState();
+}
+
+class HomePageState extends State<HomePage>{
+
+@override
+Widget build(BuildContext context){
+  return new Scaffold(
+  appBar: new AppBar(
+    title: new Text("Calculator"),
+  ),
+    body: new Container(
+      padding: const EdgeInsets.all(20.0),
+        child: new Column(
+            children: <Widget>[
+              new TextField(
+                keyboardType: TextInputType.number,
+                decoration: new InputDecoration(
+                    hintText: "Enter Number 1"
+                ),
+              ),
+              new TextField(
+                keyboardType: TextInputType.number,
+                decoration: new InputDecoration(
+                    hintText: "Enter Number 1"
+                ),
+              ),
+              new Padding(
+                  padding: const EdgeInsets.only(top: 20.0)),
+
+            ],
+        ),
+    ),
+  );
+}
+
+}
